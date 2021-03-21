532c8e5b51de2d906cfbf4df604aa755af033a90
Alan <atishin@marketing-logic.ru>
2021-03-22 01:12:15 +0300

    events in process

--- src/drawable/Drawable.ts
+++ src/drawable/Drawable.ts
@@ -1,12 +1,15 @@
+import _uniqueId from 'lodash/uniqueId'
 import DrawableOptions from 'drawable/DrawableOptions'
 import { MouseEventsTypes } from 'util/MouseEventHelper'
 import Point from 'util/Point'
 
 abstract class Drawable {
     protected options: DrawableOptions;
+    protected _key: string;
 
     constructor() {
         this.options = new DrawableOptions()
+        this._key = _uniqueId('drawable-')
     }
 
     abstract draw(ctx: CanvasRenderingContext2D): void;
